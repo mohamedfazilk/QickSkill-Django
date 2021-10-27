f86cc6dd8c569cecd6a9e2f6ff54fa0371c69ffb
mohamedfazilk <mohamedfazil961@gmail.com>
2021-10-27 14:59:56 +0530

    add signup,login in templates

--- users/views.py
+++ users/views.py
@@ -8,4 +8,4 @@ from django.contrib.auth.forms import UserCreationForm
 class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
-    template_name = 'signup.html'
+    template_name = 'users/signup.html'
