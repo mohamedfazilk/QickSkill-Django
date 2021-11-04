fea51df6b633241b369a6a8f0e07d1b731fdd317
shaheer <mohamedshaheer08@gmail.com>
2021-11-04 15:48:36 +0530

    replace istartwith by icontains

--- course/admin.py
+++ course/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+
 from course.models import skills
 
 admin.site.register(skills)
--- course/views.py
+++ course/views.py
@@ -4,17 +4,16 @@ import skills as skills
 from django.http import JsonResponse
 from django.shortcuts import render
 
-# Create your views here
 
 from course.models import skills
 
 
 def autocomplete(request):
     if 'term' in request.GET:
-        qs = skills.objects.filter(title__istartswith=request.GET.get('term'))
+        qs = skills.objects.filter(title__icontains=request.GET.get('term'))
         titles = list()
         for skill in qs:
             titles.append(skill.title)
-        # titles = [product.title for product in qs]
+
         return JsonResponse(titles, safe=False)
     return render(request, 'course/home.html')
