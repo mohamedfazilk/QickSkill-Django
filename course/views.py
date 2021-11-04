a504ce0bb659cd67f34102a18bcd9160ceae28a4
shaheer <mohamedshaheer08@gmail.com>
2021-11-04 14:54:54 +0530

    search bar with autocompletion and jquery used

--- course/views.py
+++ course/views.py
@@ -1,3 +1,20 @@
+import json
+
+import skills as skills
+from django.http import JsonResponse
 from django.shortcuts import render
 
-# Create your views here.
+# Create your views here
+
+from course.models import skills
+
+
+def autocomplete(request):
+    if 'term' in request.GET:
+        qs = skills.objects.filter(title__istartswith=request.GET.get('term'))
+        titles = list()
+        for skill in qs:
+            titles.append(skill.title)
+        # titles = [product.title for product in qs]
+        return JsonResponse(titles, safe=False)
+    return render(request, 'course/home.html')
